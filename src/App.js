0dd111fddc6195ec0708240a93df951b565909bb
Joaquin Damico <joaquindamico7@gmail.com>
2022-12-03 21:10:05 -0300

    Arreglos entrega

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import Banners from "./components/Banners";
+import NavBar from "./components/NavBar";
+import CartWidget from "./components/CartWidget";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <Header />  
+    <ItemListContainer greeting={"¡Bienvenidos a Ferreteria Servifer!"} />
+    <Banners /> 
+    <Promocion />
+    <Footer />
+  </div>
   );
-}
+} 
 
 export default App;
